d55e9db8bd09bd10ca922079fe99d812a6064ae1
SHINN\Shinn <shinn716@gmail.com>
2017-04-15 12:53:19 +0800

    creativeCoding_1

---
+++ creativeCoding_1/Assets/myCube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myCube : MonoBehaviour {
+
+    //private float size = 1;
+    public float rotateSpeed = 1.0f;
+    public Vector3 spainSpeed = Vector3.zero;
+    public Vector3 spainAxis = new Vector3(0.0f,1.0f,0);
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+        //size = Mathf.Round(Random.Range(1.5f, 3));
+        //this.transform.position = new Vector3(0,5,3);
+        //setSize(1.5f);
+        spainSpeed = new Vector3(Random.value, Random.value, Random.value);
+        //spainSpeed = new Vector3(1.0f, 1.0f, 1.0f);
+        spainAxis = Vector3.up;
+        spainAxis.x = (Random.value - Random.value)*0.1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(spainSpeed);
+        this.transform.RotateAround(Vector3.zero, spainAxis, rotateSpeed);
+	}
+
+    public void setSize(float size)
+    {
+        this.transform.localScale = new Vector3(size, size, size);
+    }
+}
---
+++ creativeCoding_1/Assets/sketch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sketch : MonoBehaviour {
+
+    public GameObject myPrefab;
+	// Use this for initialization
+	void Start () {
+        int totalCubes = 12;
+        float totalDistance =2.2f;
+
+        //Linnear 
+        //for (int i = 0; i < totalCubes; i++)
+        //{
+        //    float prec = i / (float)totalCubes;
+        //    float z = prec * totalDistance;
+        //    float y = 5.0f/3.0f* 4.0f;
+        //    float x = 0.0f;
+
+        //    var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+        //    newCube.GetComponent<myCube>().setSize(1.0f - prec);
+        //    newCube.GetComponent<myCube>().rotateSpeed = (0);
+        //}
+
+
+        //Sin
+        for (int i = 0; i < totalCubes; i++)
+        {
+            float prec = i / (float)totalCubes;
+            float sin = Mathf.Sin(prec * Mathf.PI / 2);
+            float x = 1.5f + sin * totalDistance;
+            float y = 5.0f;
+            float z = 0.0f;
+
+            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+            newCube.GetComponent<myCube>().setSize((1.0f - prec) * 0.5f);
+            newCube.GetComponent<myCube>().rotateSpeed = (0.2f + (prec))*3;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
