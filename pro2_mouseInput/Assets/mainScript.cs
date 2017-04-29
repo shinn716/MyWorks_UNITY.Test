a8d6f715171d76d5553e96a3cfcd94a71765c005
SHINN\Shinn <shinn716@gmail.com>
2017-04-29 11:34:21 +0800

    Chapter 2  ui-tween demo 照片展示+ui-tween效果

---
+++ pro2_mouseInput/Assets/mainScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainScript : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject[] g_btn = new GameObject[20];
+	Vector3[] pos = new Vector3[20];
+	Vector3[] speed = new Vector3[20];
+
+
+	public float speedValue = 0.8f;
+	// public Texture aTexture;
+
+	public static bool b_Clicked=false;
+	int stageWidth=791, stageHeight=445;
+ 	void Start () {
+
+		// GUI.DrawTexture(new Rect(321, 180, 642, 361), aTexture, ScaleMode.ScaleToFit, true, 10.0F);
+
+		for(int i=0; i<g_btn.Length; i++){
+			speed[i] = new Vector3(Random.Range(speedValue-.2f, speedValue), 0, 0);
+
+            float prec = i / (float) g_btn.Length;
+            float sin = Mathf.Sin(prec * Mathf.PI / 2);
+			float x = sin * 800;
+			float y = sin * 445;
+
+			pos[i] = new Vector3(x, y, 0);
+			// pos[i] = new Vector3(Random.Range(0, 700), Random.Range(0+40, 370-40), 0);
+
+			// print(i + " pos: "+pos[i] + " speed: "+ speed[i]);
+			g_btn[i].transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+			float rotateAngle = Random.Range(-10,10)*Random.value;
+			// print(i +" "+ rotateAngle);
+			g_btn[i].transform.Rotate(new Vector3(0, 0, rotateAngle));
+		}
+	}	
+	void Update () {
+
+		if(b_Clicked) {
+
+		} else {
+			for(int i=0; i<g_btn.Length; i++){
+				pos[i]-=speed[i];
+				g_btn[i].transform.position = pos[i];
+
+				if(pos[i].x<-80){
+	            	// float prec = i / (float) g_btn.Length;
+	            	// float sin = Mathf.Sin(prec * Mathf.PI / 2);
+					// float y = sin * 370;				
+					pos[i] = new Vector3(stageWidth+80, Random.Range(0, stageHeight-40), 0);
+				}
+			}
+		}
+
+
+
+	}
+
+}
---
+++ pro2_mouseInput/Assets/rootScript.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(UnityEngine.EventSystems.EventTrigger))]
+public class rootScript : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject g_BG;
+	public GameObject g_BGShow;
+	// public EasyTween[] theTween;
+	int beenClicked=0;
+
+	// imgScript myImgScript = new imgScript();
+	string[] fileName = {
+		"almond-1201795_640", "bee-on-cherry-blossoms-1403010_640", "checkered-butterfly-1472687_640", "cosmea-1396842_640",
+		"dandelion-1346727_640", "drip-1972411_640", "flower-139237_640", "flower-624974_640",
+		"mushrooms-2212899_640", "tomatoes-1561565_640", "n (1)", "n (2)",
+		"n (3)", "n (4)", "n (5)", "n (6)",
+		"n (7)", "n (8)", "n (9)", "n (10)"
+	};
+
+	public Image imageChange;
+	public Text showContent;
+	void Start(){
+		g_BG.SetActive(false);
+		g_BGShow.SetActive(false);
+		// imageChange.sprite = Resources.Load<Sprite>(fileName[0]);
+	}
+	void Update(){
+
+	}
+
+	public void enter(){
+
+		// g_BGShow.SetActive(true);
+		print("who: " + beenClicked);
+		mainScript.b_Clicked = true;
+		
+		string tmp = fileName[beenClicked-1];
+		imageChange.sprite = Resources.Load<Sprite>(tmp);
+		
+	} 
+
+	public void closeBG(){
+		g_BG.SetActive(false);
+		g_BGShow.SetActive(false);
+		mainScript.b_Clicked = false;
+		// showImage.SetActive(false);
+	}
+
+	public void callBtn1(){
+		beenClicked=1;
+		showContent.text = "Title: 櫻花 \n almond-1201795_640";
+	}
+	public void callBtn2(){
+		beenClicked=2;
+	}
+	public void callBtn3(){
+		beenClicked=3;
+	}
+	public void callBtn4(){
+		beenClicked=4;
+	}
+	public void callBtn5(){
+		beenClicked=5;
+	}
+	public void callBtn6(){
+		beenClicked=6;
+	}
+	public void callBtn7(){
+		beenClicked=7;
+	}
+	public void callBtn8(){
+		beenClicked=8;
+	}
+	public void callBtn9(){
+		beenClicked=9;
+	}
+	public void callBtn10(){
+		beenClicked=10;
+	}
+	public void callBtn11(){
+		beenClicked=11;
+	}
+	public void callBtn12(){
+		beenClicked=12;
+	}
+	public void callBtn13(){
+		beenClicked=13;
+	}
+	public void callBtn14(){
+		beenClicked=14;
+	}
+	public void callBtn15(){
+		beenClicked=15;
+	}
+	public void callBtn16(){
+		beenClicked=16;
+	}
+	public void callBtn17(){
+		beenClicked=17;
+	}
+	public void callBtn18(){
+		beenClicked=18;
+	}
+	public void callBtn19(){
+		beenClicked=19;
+	}
+	public void callBtn20(){
+		beenClicked=20;
+	}
+
+
+    // public IEnumerator animBtn1()
+    // {
+	// 	print("show");
+    //     theTween[0].OpenCloseObjectAnimation();
+    //     yield return new WaitForSecondsRealtime(0.5f);
+    // }
+}
